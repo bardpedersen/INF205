8fa66da5e9abbc824b8ffdb5f13919898ad25e5a
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2022-12-05 19:19:29 +0100

    Merged MPI from tom

---
+++ MPI_Monte_Carlo/box.h
@@ -0,0 +1,35 @@
+#ifndef BOX_H
+#define BOX_H
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "sphere.h"
+
+class Box
+{
+public:
+   void in(std::istream* source);  // read from *source
+   
+   long count_collisions();  // count number of collisions
+
+   int move_sphere(int number_of_coll,int i); // move spheres
+   
+   int get_N() const{return this->N;};
+
+   std::multimap<double, int, std::greater<int>> &get_components() {return this->components;};
+   std::vector<std::vector<Sphere>> &get_particles() {return this->particles;};
+
+   
+
+private:
+   double extension[3];  // size of the box
+   std::multimap<double, int, std::greater<int>> components;  //  IDs (value) ordered by size (key)
+   std::vector<std::vector<Sphere>> particles;  // maps each component ID to the spheres associated with that component
+   size_t N = 0;  // total number of particles
+};
+
+std::istream& operator>>(std::istream& is, Box& b);
+
+#endif
